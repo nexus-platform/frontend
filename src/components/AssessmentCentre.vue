75ccad47fec0636364508681b15c90b7cdcead53
fdbatista <fdbatista@gmail.com>
2018-09-03 12:53:04 -0400

    AC management

--- src/components/AssessmentCentre.vue
+++ src/components/AssessmentCentre.vue
@@ -24,27 +24,82 @@
       </v-layout>
 
       <template v-if="!loadingInitialElements">
-        
         <template v-if="!ac.registered">
           <v-container pa-5>
+            <v-form v-model="validationStatus" ref="form">
+              <v-layout row wrap>
+                <v-flex sm5 md5>
+                  <v-text-field :disabled="!isGuest" v-model="ac.user_data.name" prepend-icon="edit" name="name" label="First name" type="text" :rules="nameRules"></v-text-field>
+                  <v-text-field :disabled="!isGuest" v-model="ac.user_data.last_name" prepend-icon="edit" name="last_name" label="Last name" type="text" :rules="lastNameRules"></v-text-field>
+                  <v-text-field :disabled="!isGuest" @blur="test" v-model="ac.user_data.postcode" prepend-icon="place" name="postcode" label="Postcode" :rules="postcodeRules" :hint="loadingPostcodeInfo ? 'Loading postcode...' : 'Enter a postcode to lookup'" type="text"></v-text-field>
+                  <v-text-field :disabled="!isGuest" v-model="ac.user_data.address" prepend-icon="fas fa-map-signs" name="address" label="Address" id="address" type="text" :rules="addressRules"></v-text-field>
+                </v-flex>
+                <v-flex offset-sm2 sm5>
+                  <v-text-field :disabled="!isGuest" v-model="ac.user_data.email" prepend-icon="email" name="email" label="Email" id="email" type="email" :rules="emailRules"></v-text-field>
+                  <v-text-field :disabled="!isGuest" v-model="ac.user_data.password" prepend-icon="lock" name="password" label="Password" id="password" :rules="passwordRules" :append-icon="e1 ? 'visibility' : 'visibility_off'" @click:append="() => (e1 = !e1)" :type="e1 ? 'password' : 'text'" hint="At least 6 characters" min="6"></v-text-field>
+                  <v-text-field :disabled="!isGuest" v-model="ac.user_data.password_confirm" prepend-icon="lock" name="passwordConfirm" label="Password Confirmation" id="passwordConfirm" :rules="passwordConfirmRules" :append-icon="e1 ? 'visibility' : 'visibility_off'" @click:append="() => (e1 = !e1)" :type="e1 ? 'password' : 'text'" hint="Re-type your password"></v-text-field>
+                  <v-text-field :readonly="true" v-model="dsaLetterName" label="DSA Letter" :rules="dsaLetterRules" prepend-icon="attach_file" append-icon="folder" @click="uploadDialog = true" @click:append="() => (uploadDialog = true)" type="text" hint="Upload a copy of your DSA letter"></v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row wrap mt-5>
+                <v-flex xs12>
+                  <v-tooltip bottom :color="validationColor">
+                    <v-btn :disabled="loading" v-on:click="submit" class="white--text" :class="{ red: !validationStatus, indigo: validationStatus }" slot="activator">
+                      <icon v-if="loading" name="circle-notch" spin class="gray--text"></icon>
+                      <v-icon size="22" v-if="!loading && validationStatus">done</v-icon>
+                      <v-icon size="22" v-if="!loading && !validationStatus">error_outline</v-icon>
+                      &nbsp;Register with this Centre
+                    </v-btn>
+                    <span>{{validationMessage}}</span>
+                  </v-tooltip>
+                </v-flex>
+              </v-layout>
+            </v-form>
+          </v-container>
+
+          <v-dialog width="500" v-model="uploadDialog" persistent>
+            <v-card>
+              <v-card-title class="headline grey lighten-2">
+                Upload file
+                <v-spacer></v-spacer>
+                <a @click="uploadDialog = false"><icon name="times" class="fa"></icon></a>
+              </v-card-title>
+              <v-container>
+                <file-upload v-on:set-file="setDSALetter($event)"></file-upload>
+              </v-container>
+            </v-card>
+          </v-dialog>
+        </template>
+
+        <template v-else-if="ac.registered && !ac.is_admin">
+          <v-container>
             <v-layout row wrap>
               <v-flex sm5 md5>
-                <v-text-field :disabled="!isGuest" v-model="ac.user_data.name" prepend-icon="edit" name="name" label="First name" type="text" :rules="nameRules"></v-text-field>
-                <v-text-field :disabled="!isGuest" v-model="ac.user_data.last_name" prepend-icon="edit" name="last_name" label="Last name" type="text" :rules="lastNameRules"></v-text-field>
-                <v-text-field :disabled="!isGuest" @blur="test" v-model="ac.user_data.postcode" prepend-icon="place" name="postcode" label="Postcode" :hint="loadingPostcodeInfo ? 'Loading postcode...' : 'Enter a postcode to lookup'" single-line id="postcode" type="text"></v-text-field>
-                <v-text-field :disabled="!isGuest" v-model="ac.user_data.address" prepend-icon="fas fa-map-signs" name="address" label="Address" id="address" type="text" :rules="addressRules"></v-text-field>
+                <v-btn color="error" @click="test()"><icon class="fa" name="times"></icon> Cancel Registration</v-btn>
               </v-flex>
-              <v-flex offset-sm2 sm5 offset-md2 md5>
-                <v-text-field :disabled="!isGuest" v-model="ac.user_data.email" prepend-icon="email" name="email" label="Email" id="email" type="email" :rules="emailRules"></v-text-field>
-                <v-text-field :disabled="!isGuest" v-model="ac.user_data.password" prepend-icon="lock" name="password" label="Password" id="password" :rules="passwordRules" :append-icon="e1 ? 'visibility' : 'visibility_off'" @click:append="() => (e1 = !e1)" :type="e1 ? 'password' : 'text'" hint="At least 6 characters" min="6"></v-text-field>
-                <v-text-field :disabled="!isGuest" v-model="ac.user_data.password_confirm" prepend-icon="lock" name="passwordConfirm" label="Password Confirmation" id="passwordConfirm" :rules="passwordConfirmRules" :append-icon="e1 ? 'visibility' : 'visibility_off'" @click:append="() => (e1 = !e1)" :type="e1 ? 'password' : 'text'" hint="Re-type your password"></v-text-field>
-                <file-select v-on:file-changed="setDSALetter($event)"></file-select>
+              <v-flex offset-sm1 sm5>
+                <v-text-field :readonly="true" v-model="dsaLetterName" label="DSA Letter" :rules="dsaLetterRules" prepend-icon="attach_file" append-icon="folder" @click="uploadDialog = true" @click:append="() => (uploadDialog = true)" type="text" hint="Upload a copy of your DSA letter"></v-text-field>
               </v-flex>
             </v-layout>
           </v-container>
         </template>
 
+        <template v-else-if="ac.registered && ac.is_admin">
+          <v-container>
+            <v-layout row wrap>
+              <v-flex>
+                <v-btn color="primary" @click="test()"><icon class="fa" name="envelope"></icon> Invite users</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </template>
+        
       </template>
+
+      <v-snackbar :timeout="5000" :bottom="true" :right="true" v-model="snackbar" :color="operationMessageType">
+        <icon class="fa" name="info-circle"></icon> {{ operationMessage }}
+        <v-btn flat @click.native="snackbar = false"><icon name="times"></icon></v-btn>
+      </v-snackbar>
 
     </v-card>
 	</v-container>
@@ -52,26 +107,30 @@
 
 <script>
 import axios from "axios";
-import FileSelect from "@/components/FileSelect";
+import FileUpload from "@/components/FileUpload";
 
 export default {
   data() {
     return {
       e1: true,
+      uploadDialog: false,
+      validationStatus: false,
       loadingInitialElements: true,
       acSlug: null,
-      apiUrl: null,
       acAction: null,
+      snackbar: false,
+      loading: false,
       loadingPostcodeInfo: false,
       nameRules: [v => !!v || "This field is required"],
       lastNameRules: [v => !!v || "This field is required"],
+      dsaLetterRules: [v => !!v || "This field is required"],
       passwordRules: [
         v => !!v || "This field is required",
         v => (v && v.length > 5) || "Password requires at least 6 characters"
       ],
       passwordConfirmRules: [
         v => !!v || "This field is required",
-        v => v === this.password || "Passwords do not match"
+        v => v === this.ac.user_data.password || "Passwords do not match"
       ],
       emailRules: [
         v => !!v || "E-mail is required",
@@ -79,9 +138,11 @@ export default {
           /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
           "E-mail must be valid"
       ],
+      postcodeRules: [v => !!v || "This field is required"],
       addressRules: [v => !!v || "This field is required"],
-      operationMessage: "Enter  your credentials",
+      operationMessage: "Enter your credentials",
       operationMessageType: "warning",
+      dsaLetterName: "",
       ac: {
         id: null,
         name: "Loading Assessment Centre information",
@@ -92,15 +153,22 @@ export default {
     };
   },
   created() {
-    this.apiUrl =
-      this.$store.state.baseUrl +
-      "send-dsa-form-comment?XDEBUG_SESSION_START=netbeans-xdebug";
+    this.activationUrl = window.location.href.replace(
+      this.$route.path,
+      "/activate-account"
+    );
     this.setInitialData();
   },
-  components: { FileSelect },
+  components: { FileUpload },
   computed: {
     isGuest() {
       return this.$store.state.payload.is_guest;
+    },
+    validationMessage: function() {
+      return this.validationStatus ? "Good to go!" : "The form is incomplete";
+    },
+    validationColor: function() {
+      return this.validationStatus ? "indigo" : "red";
     }
   },
   methods: {
@@ -125,13 +193,67 @@ export default {
       });
     },
     setDSALetter(event) {
-      console.log('Yea');
+      this.ac.user_data.dsa_letter = event;
+      if (event) {
+        this.dsaLetterName = event.name;
+        this.uploadDialog = false;
+      } else {
+        this.dsaLetterName = "";
+      }
+    },
+    submit() {
+      if (this.$refs.form.validate()) {
+        this.loading = true;
+        var requestConfig = {
+          headers: {
+            Authorization: "Bearer " + this.$store.state.payload.jwt,
+            "Content-Type": "multipart/form-data"
+          }
+        };
+        var formData = new FormData();
+        formData.append("dsa_letter", this.ac.user_data.dsa_letter);
+        formData.append("ac", JSON.stringify(this.ac));
+        formData.append("url", this.activationUrl);
+
+        var that = this;
+        axios
+          .post(
+            this.$store.state.baseUrl +
+              "register-with-ac?XDEBUG_SESSION_START=netbeans-xdebug",
+            formData,
+            requestConfig
+          )
+          .then(function(response) {
+            that.loading = false;
+            that.operationMessage = response.data.msg;
+            that.operationMessageType = response.data.code;
+            that.snackbar = true;
+            if (response.data.code === "success") {
+              that.ac.registered = true;
+              if (that.isGuest) {
+                that.$store.state.authRouteRequested =
+                  "/assessment-centre/" + that.acSlug;
+                setTimeout(function() {
+                  that.$router.push("/login");
+                }, 5000);
+              } else {
+                that.$router.push("/assessment-centre/" + that.acSlug);
+              }
+            }
+          })
+          .catch(function(error) {
+            that.loading = false;
+            that.operationMessage =
+              "There was an error on the remote endpoint. Try again later.";
+            that.operationMessageType = "error";
+            that.snackbar = true;
+          });
+      }
     },
     test() {
       console.log("Test");
     },
     getAC() {
-      this.loading = true;
       return new Promise((resolve, reject) => {
         var requestConfig = {
           headers: {
---
+++ src/components/FileUpload.vue
@@ -0,0 +1,116 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 v-if="!file">
+        <div class="box">
+          <input type="file" @change="onFileChange" name="file-5" id="file-5" class="inputfile inputfile-4" />
+          <label for="file-5"><figure><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg></figure> <span>Select a file to upload</span></label>
+        </div>
+      </v-flex>
+      <v-flex xs12 v-else>
+        <v-flex>
+          <label>{{file.name}}</label>
+          <v-divider></v-divider>
+        </v-flex>
+        <v-flex>
+          <v-btn color="warning" v-on:click="removeFile()"><icon name="trash" class="fa"></icon>Discard</v-btn>
+          <v-btn color="info" v-on:click="setFile()"><icon name="check" class="fa"></icon> Use this file</v-btn>
+        </v-flex>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      file: null
+    };
+  },
+  methods: {
+    onFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      this.file = files[0];
+    },
+    removeFile() {
+      this.file = null;
+      this.$emit("set-file", this.file);
+    },
+    setFile() {
+      this.$emit("set-file", this.file);
+    }
+  }
+};
+</script>
+
+<style scoped>
+label {
+  display: flex;
+  align-self: flex-start;
+  white-space: normal;
+  height: inherit;
+}
+.inputfile {
+  width: 0.1px;
+  height: 0.1px;
+  opacity: 0;
+  overflow: hidden;
+  position: absolute;
+  z-index: -1;
+}
+.inputfile:focus + label,
+.inputfile + label:hover {
+  background-color: #dfc8ca;
+}
+.inputfile:focus + label {
+  outline: 1px dotted #000;
+  outline: -webkit-focus-ring-color auto 5px;
+}
+.box {
+  padding: 1.25rem 1.25rem;
+}
+.inputfile + label {
+  max-width: 80%;
+  font-size: 1.25rem;
+  font-weight: 700;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  cursor: pointer;
+  display: inline-block;
+  overflow: hidden;
+  padding: 0.625rem 1.25rem;
+}
+.inputfile + label svg {
+  width: 1em;
+  height: 1em;
+  vertical-align: middle;
+  fill: currentColor;
+  margin-top: -0.25em;
+  margin-right: 0.25em;
+}
+.inputfile-4 + label {
+  color: #d3394c;
+}
+.inputfile-4:focus + label,
+.inputfile-4.has-focus + label,
+.inputfile-4 + label:hover {
+  color: #722040;
+}
+.inputfile-4 + label figure {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: #d3394c;
+  display: block;
+  padding: 20px;
+  margin: 0 auto 10px;
+}
+.inputfile-4 + label svg {
+  width: 100%;
+  height: 100%;
+  fill: #f1e5e6;
+}
+</style>
